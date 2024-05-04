abdb77db2be754707328f23e6b208853d36f59d4
Dylan Vukelich <dylanmvukelich@gmail.com>
2024-05-04 17:21:01 -0500

    humidity added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (longitude, latitude, callback) => {
 		} else if (body.error) {
 			callback("Unable to find location", undefined);
 		} else {
-			callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out.");
+			callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out. The humidity is " + body.current.humidity + "%.");
 		};
 	});
 	//if there is no network connection, response will be undefined, so with no error handeling you'll get the default error saying can't read body of undefined.
