a7bc93bb1a5d60505b4243da805f69c82285cbe6
Dylan Vukelich <dylanmvukelich@gmail.com>
2024-05-04 18:03:44 -0500

    spelling mistake corrected

--- src/app.js
+++ src/app.js
@@ -79,7 +79,7 @@ app.get("/weather", (req, res) => {
 			return res.send({error});
 		};
 		
-		forecast(latitude, lognitude, (error, forecastData) => {
+		forecast(latitude, longitude, (error, forecastData) => {
 			if (error) {
 				return res.send({error});
 			};
